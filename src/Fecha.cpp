bfb72e2fbce14da2189f44671a078f31831bc786
elarle <arbol@git.ak>
2026-02-23 09:48:41 +0100

    Añadida la clase fecha (no tiene nada dentro)

---
+++ src/Fecha.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fecha.cpp
+ *
+ *  Created on: Feb 16, 2026
+ *      Author: arbol
+ */
+
+#include "Fecha.h"
+
+Fecha::Fecha() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Fecha::~Fecha() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Fecha.h
@@ -0,0 +1,17 @@
+/*
+ * Fecha.h
+ *
+ *  Created on: Feb 16, 2026
+ *      Author: arbol
+ */
+
+#ifndef FECHA_H_
+#define FECHA_H_
+
+class Fecha {
+public:
+	Fecha();
+	virtual ~Fecha();
+};
+
+#endif /* FECHA_H_ */
