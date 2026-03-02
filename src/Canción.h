6d2a5a94b3dfdff2a59b980d019b26a3c28c39c9
estudiante <cerberux444@gmail.com>
2026-03-02 10:59:03 +0100

    Cabecera de la clase Cancion

--- src/Canción.h
+++ src/Canción.h
@@ -8,6 +8,47 @@
 #ifndef CANCIÓN_H_
 #define CANCIÓN_H_
 
+#include <iostream>
+using namespace std;
 
+class Cancion {
+	private:
+		string titulo;
+		string genero;
+		int duracion;
+	public:
+		Cancion();
+		Cancion(string titulo, string genero, int duracion);
+		Cancion(const Cancion & cancion);
+		~Cancion();
+
+		//Getters
+		string getTitulo() const;
+		string getGenero() const;
+		int getDuracion() const;
+
+		//Setters
+		void setTitulo(string titulo);
+		void setCGenero(string genero);
+		void setDuracion(int duracion);
+
+		//Operadores
+		/**
+		 * DESC={Compara el titulo de las canciones}
+		 * COMP={O(1)}
+		 */
+		bool operator==(const Cancion &cancion) const;
+
+		/**
+		 * DESC={
+		 *		Compara los titulos de las canciones según el órden alfabético.
+		 *		Devuelve true si el primero estaría antes afabéticamente.
+		 *		Si devuelve false el segundo operando estaría primero.
+		 * }
+		 * COMP={O(1)}
+		 */
+		bool operator<(const Cancion &cancion) const;
+
+};
 
 #endif /* CANCIÓN_H_ */
