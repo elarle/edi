ce7dc0865cfcdb87faf6eaf376d3bfa02f17eed7
estudiante <cerberux444@gmail.com>
2026-03-02 11:14:48 +0100

    Cabecera de las pruebas de la clase Canción

---
+++ src/CanciónTest.h
@@ -0,0 +1,17 @@
+/*
+ * CanciónTest.h
+ *
+ *  Created on: 2 mar 2026
+ *      Author: estudiante
+ */
+
+#ifndef CANCIÓNTEST_H_
+#define CANCIÓNTEST_H_
+#include "Canción.h"
+
+void pruebasCancion();
+
+void pruebasConstrucoresCancion();
+void pruebasOperadoresCancion();
+
+#endif /* CANCIÓNTEST_H_ */
