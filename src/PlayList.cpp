e29436e6187ebcf97d5cfb6a2d10de82e2729816
estudiante <cerberux444@gmail.com>
2026-03-09 11:44:27 +0100

    Implementación de Reproducir, ReproducirDesde, Constructor y el Inicio de Mostrar la cola (que no sé bien como hacerlo)

--- src/PlayList.cpp
+++ src/PlayList.cpp
@@ -2,16 +2,27 @@
 
 //Constructores
 PlayList::PlayList(){
+	this->nombre = "";
+	this->canciones = 0;
+	this->duracion = 0;
+
+	cola = new Cola<Cancion*>;
 
 };
+
 PlayList::PlayList(string nombre){
+	this->nombre = nombre;
+	this->canciones = 0;
+	this->duracion = 0;
+
+	cola = new Cola<Cancion*>;
 
 }
 PlayList::PlayList(const PlayList & playlist){
 
 }
 PlayList::~PlayList(){
-
+	delete cola;
 }
 
 //Getters
@@ -32,6 +43,15 @@ void PlayList::setNombre(string nombre){
 
 //Métodos
 void PlayList::mostrar() const{
+	Cancion *c = nullptr;
+	Cola<Cancion*> *aux = new Cola<Cancion*>;
+
+	while (!cola->estaVacia()){
+		c = cola->getPrimero (c);
+		c->mostrar ();
+		aux->encolar(c);
+		cola->desencolar ();
+	}
 
 }
 
@@ -44,9 +64,24 @@ void PlayList::eliminarCancion(){
 }
 
 void PlayList::reproducir(){
+	int i;
+	Cancion *c = nullptr;
 
+	for (i = 0 ; i < this->canciones; i++){
+		c = cola->getPrimero (c);
+		c->mostrar();
+		cola->desencolar ();
+		cola->encolar (c);
+	}
 }
 
 void PlayList::reproducirDesde(int indice){
-
+	int i;
+	Cancion *c = nullptr;
+	for (i = 0 ; i < indice ; i++){
+		c = cola->getPrimero (c);
+		cola->desencolar ();
+		cola->encolar (c);
+	}
+	reproducir();
 }
