6a42cba1b7273e5f3a4770191c3d6bf604631e87
estudiante <cerberux444@gmail.com>
2026-04-13 11:23:41 +0200

    Cabecera de GestorArtistas

---
+++ src/GestorArtistasTest.h
@@ -0,0 +1,19 @@
+/*
+ * GestorArtistaTest.h
+ *
+ *  Created on: 13 abr 2026
+ *      Author: estudiante
+ */
+
+#ifndef GESTORARTISTASTEST_H_
+#define GESTORARTISTASTEST_H_
+#include "GestorArtistas.h"
+
+void pruebasGestorArtistas();
+
+void pruebaConstructoresGestorArtistas();
+void pruebaNumElementosGestorArtistas();
+void pruebaBuscarGestorArtistas();
+void pruebaInsertarGestorArtistas();
+
+#endif /* GESTORARTISTASTEST_H_ */
