4c3cc6b10ea187a1a9b78b8b08440e26e171fd01
elarle <arbol@git.ak>
2026-04-20 09:18:27 +0200

    Añadida la variante de Árboles binarios.

--- src/GestorUsuarios.h
+++ src/GestorUsuarios.h
@@ -1,8 +1,12 @@
 #ifndef GESTOR_USUARIOS_H_
 #define GESTOR_USUARIOS_H_
 
-#include "ListaDPI.h"
 #include "Usuario.h"
+
+#define LISTA_USUARIOS
+
+#if defined LISTA_USUARIOS
+#include "ListaDPI.h"
 
 class GestorUsuarios{
 	private:
@@ -40,5 +44,42 @@ class GestorUsuarios{
 		void mostrar() const;
 
 };
+
+#else /* LISTA_USUARIOS */
+#include "BSTree.h"
+
+class GestorUsuarios{
+	private:
+		BSTree<Usuario*>* usuarios;
+		int numero_usuarios;
+		
+	public:
+		GestorUsuarios();
+		GestorUsuarios(const GestorUsuarios & g);
+		~GestorUsuarios();
+
+		/**
+		 * DESC={Devuelve el número de usuarios registrados}
+		 * COMP={O(1)}
+		 */
+		int numElementos() const;
+		/**
+		 * DESC={Devuelve true si hay algún usuario cuyo nombre coincida}
+		 * COMP={O(log(n))}
+		 */
+		bool buscar(string nombre, Usuario* &usuario) const;
+		
+		/**
+		 * TODO: Comprobar que siga el orden el test
+		 * DESC={Inserta un nuevo usuario en órden si no se repite ni el id, nombre ni email}
+		 * COMP={O(log(n))}
+		 */
+		void insertar(string id, string nombre, string email, string contraseña, Fecha* fecha);
+		
+		void mostrar() const;
+
+};
+
+#endif /* LISTA_USUARIOS */
 
 #endif /* GESTOR_USUARIOS_H_ */
