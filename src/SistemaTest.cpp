a71568cac0f8eb6c0704bc50c2ff1c47b5edbd3b
elarle <arbol@git.ak>
2026-05-03 14:19:05 +0200

    Mejorada la prueba de buscarUsuario de Sistema.

--- src/SistemaTest.cpp
+++ src/SistemaTest.cpp
@@ -37,16 +37,30 @@ void pruebasBuscarUsuarioSistema(){
 	s1 = new Sistema();
 	
 	//DUDA: Debemos devolver una copia o el usuario directo?
-	Usuario* busqueda = s1->buscarUsuario("DanielJ");
+	Usuario* busqueda = s1->buscarUsuario("Coronado Perez, Pablo");
 	if(busqueda == nullptr)
 		cerr << " * Error con la primera búsquea." << endl;
-	//TODO: Comprobar que el usuario sea el correcto
+	else if(busqueda->getIdUsuario() != "PabloCa")
+		cerr << " * Error con la primera búsqueda. Usuario incorrecto." << endl;
+
+	busqueda = s1->buscarUsuario("Sanchez Mancera, Andres");
+	if(busqueda == nullptr)
+		cerr << " * Error con la segunda búsquea." << endl;
+	else if(busqueda->getIdUsuario() != "AndresS")
+		cerr << " * Error con la segunda búsqueda. Usuario incorrecto." << endl;
+
+	busqueda = nullptr;
+	busqueda = s1->buscarUsuario("Perikillo");
+
+	if(busqueda != nullptr)
+		cerr << " * Error con la tercera búsqueda." << endl;
 
 	delete s1;
 
 	cout << "FinalizadaS pruebas de constructores de Sistema" << endl;
 }
 
+//TODO: Añadir más casos
 void pruebasBuscarArtistaSistema(){
 	cout << "Iniciando pruebas de buscarArtista de Sistema" << endl;
 	
@@ -64,20 +78,18 @@ void pruebasBuscarArtistaSistema(){
 	cout << "FinalizadaS pruebas de constructores de Sistema" << endl;
 }
 
+//TODO: Esta prueba está mal hecha, esto se va para usuario
 void pruebasReproducirPlaylistUsuarioSistema(){
 	cout << "Iniciando pruebas de de ReproducirPlaylistUsuarioSistema" << endl;
 
 	Sistema* s = new Sistema();
 	Usuario* u = s->buscarUsuario("Coronado Perez, Pablo");
 
-
-	if(u == nullptr){
-		cerr << " * Prueba vacía" << endl;
-	}
-	else{
+	if(u != nullptr){
 		u->crearPlayList("TestSistema");
 		u->reproducirPlayList("TestSistema");
 	}
+
 	//NO SE HACE DELETE DEL USUARIO POR QUE ES DE SISTEMA
 	//delete s;
 	delete u;
