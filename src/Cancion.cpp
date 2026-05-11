8e78d69805d1418caa0e7fd5e30fbd21bc2769c2
elarle <elarle@git.ak>
2026-05-11 22:40:45 +0200

    Mejor formato de las canciones

--- src/Cancion.cpp
+++ src/Cancion.cpp
@@ -55,11 +55,9 @@ void Cancion::setDuracion(int duracion){
 }
 
 void Cancion::mostrar() const{
-	cout	<< "Canción{" << endl
-			<< "	título: "	<< this->titulo	<< endl
-			<< "	género: "	<< this->genero	<< endl
-			<< "	duración: " << this->duracion << endl
-			<< "}" << endl;
+	cout << this->titulo	<< ". " 
+		<< this->genero 
+		<< ". Duración: "<< this->duracion << "s" << endl;
 }
 
 bool Cancion::operator==(const Cancion &cancion) const{
