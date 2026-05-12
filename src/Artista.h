18acb1af1f39c3db1c4fd1982efa9ccf3998becd
elarle <elarle@git.ak>
2026-05-12 19:16:40 +0200

    Especificación de artista checkeada

--- src/Artista.h
+++ src/Artista.h
@@ -51,12 +51,33 @@ class Artista {
 		bool operator<(const Artista &artista) const;
 
 		//Métodos
+		/**
+		 * DESC={Busca una canción por título en la lista del artista}
+		 * POST={Si se encuentra, se devuelve true y se asigna el puntero. Si no, no se modifica.}
+		 * COMP={O(n)}
+		 */
 		bool buscarCancion(string titulo, Cancion* &cancion) const;
+		/**
+		 * DESC={Inserta una canción en el artista si no existe ya una con ese título}
+		 * COMP={O(n)}
+		 */
 		void insertarCancion(string titulo, string genero, int duracion);
 
+		/**
+		 * DESC={Incrementa el número de seguidores}
+		 * COMP={O(1)}
+		 */
 		void addSeguidor();
+		/**
+		 * DESC={Decrementa el número de seguidores si es mayor que 0}
+		 * COMP={O(1)}
+		 */
 		void delSeguidor();
 
+		/**
+		 * DESC={Muestra la información del artista y sus canciones}
+		 * COMP={O(n)}
+		 */
 		void mostrar() const;
 
 };
