64842f6e6bdcb7d9e01607c9c08133ea7d0c2ac6
elarle <elarle@git.ak>
2026-05-13 20:05:33 +0200

    Arreglo del merge

--- src/GestorUsuarios.h
+++ src/GestorUsuarios.h
@@ -58,41 +58,6 @@ class GestorUsuarios{
 		GestorUsuarios(const GestorUsuarios & g);
 		~GestorUsuarios();
 
-		/**
-		 * DESC={Devuelve el número de usuarios registrados}
-		 * COMP={O(1)}
-		 */
-		int numElementos() const;
-		/**
-		 * DESC={Devuelve true si hay algún usuario cuyo nombre coincida}
-		 * COMP={O(log(n))}
-		 */
-		bool buscar(string nombre, Usuario* &usuario) const;
-		
-		/**
-		 * TODO: Comprobar que siga el orden el test
-		 * DESC={Inserta un nuevo usuario en órden si no se repite ni el id, nombre ni email}
-		 * COMP={O(log(n))}
-		 */
-		void insertar(string id, string nombre, string email, string contraseña, Fecha* fecha);
-		
-		void mostrar() const;
-
-};
-
-#else /* LISTA_USUARIOS */
-#include "BSTree.h"
-
-class GestorUsuarios{
-	private:
-		BSTree<Usuario*>* usuarios;
-		int numero_usuarios;
-		
-	public:
-		GestorUsuarios();
-		GestorUsuarios(const GestorUsuarios & g);
-		~GestorUsuarios();
-
 		void copiarArbol(BSTree<Usuario*> *a1, BSTree<Usuario*> *a2);
 
 		void destruirUsuarios(BSTree<Usuario*> *a);
@@ -124,5 +89,4 @@ class GestorUsuarios{
 };
 
 #endif /* LISTA_USUARIOS */
-
 #endif /* GESTOR_USUARIOS_H_ */
